fac15f1c7469a9b22694fe2ad103345d2c45e594
munmud <moontasir042@gmail.com>
2023-04-08 22:12:04 +0600

    added c

---
+++ age_community_detection/lib/community_detection.h
@@ -0,0 +1,8 @@
+#ifndef COMMUNITY_DETECTION_H
+#define COMMUNITY_DETECTION_H
+
+void get_community(int *input,
+                   int nrows,
+                   int ncols,
+                   int *output);
+#endif
